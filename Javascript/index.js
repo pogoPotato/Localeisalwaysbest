d0a02865a094d839e34e84bf33b174f85d661df0
RIzZz <95970726+pogoPotato@users.noreply.github.com>
2026-05-13 18:15:09 +0900

    Delete Javascript directory

--- Javascript/index.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById("menu-icon");
-const navbar = document.getElementById("navbar");
-
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
